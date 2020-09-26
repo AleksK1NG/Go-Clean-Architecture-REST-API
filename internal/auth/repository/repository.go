dc3ca9bbe4060e8fe1c115f0c315bc5ea07d95bd
Alexander Bryksin <abryksin@luxoft.com>
2020-09-26 17:05:20 +0300

    add create user handler

--- internal/auth/repository/repository.go
+++ internal/auth/repository/repository.go
@@ -2,9 +2,11 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"github.com/AleksK1NG/api-mc/internal/logger"
 	"github.com/AleksK1NG/api-mc/internal/models"
 	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
 )
 
 // Auth Repository
@@ -20,10 +22,21 @@ func NewAuthRepository(logger *logger.Logger, db *sqlx.DB) *repository {
 
 // Create new user
 func (r *repository) Create(ctx context.Context, user *models.User) (*models.User, error) {
-	// createUserQuery := `INSERT INTO users (first_name, last_name, email, password, role, about, avatar, phone_number, address,
-	//                		city, role, gender, postcode, birthday, created_at, updated_at, login_date)
-	// 					VALUES (:first_name, :last_name, :email, :password, :role, :about, :avatar, :phone_number, :address,
-	//     				:city, :role, :gender, :postcode, :birthday, now(), now(), now())`
-	return user, nil
+
+	createUserQuery := `INSERT INTO users (first_name, last_name, email, password, role, about, avatar, phone_number, address,
+	               		city, gender, postcode, birthday, created_at, updated_at, login_date)
+						VALUES ($1, $2, $3, $4, COALESCE($5, 'user'), $6, $7, $8, $9, $10, $11, $12, $13, now(), now(), now()) RETURNING *`
+
+	var u models.User
+	if err := r.db.QueryRowxContext(ctx, createUserQuery, &user.FirstName, &user.LastName, &user.Email, &user.Password, &user.Role,
+		&user.About, &user.Avatar, &user.PhoneNumber, &user.Address, &user.City, &user.Gender, &user.Postcode, &user.Birthday,
+	).StructScan(&u); err != nil {
+		r.logger.Error("QueryRowxContext", zap.String("ERROR", err.Error()))
+		return nil, err
+	}
+
+	r.logger.Info("USER", zap.String("USER", fmt.Sprintf("%#v", u)))
+
+	return &u, nil
 
 }
