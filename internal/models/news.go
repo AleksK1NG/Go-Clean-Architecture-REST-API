7db9178ebb60c2b828474c587236c50889d31584
Alexander Bryksin <abryksin@luxoft.com>
2020-10-06 20:03:16 +0300

    add news create handler

--- internal/models/news.go
+++ internal/models/news.go
@@ -8,7 +8,7 @@ import (
 // News base model
 type News struct {
 	ID        uuid.UUID `json:"news_id" db:"news_id" validate:"omitempty,uuid"`
-	AuthorID  uuid.UUID `json:"author_id" db:"author_id" validate:"omitempty,uuid"`
+	AuthorID  uuid.UUID `json:"author_id" db:"author_id" validate:"required"`
 	Title     string    `json:"title" db:"title" validate:"required,gte=10"`
 	Content   string    `json:"content" db:"content" validate:"required,gte=20"`
 	ImageURL  *string   `json:"image_url,omitempty" db:"image_url" validate:"omitempty,lte=512,url"`
