228e5542b43e110d00954af89a73bddbe534d37f
Alexander Bryksin <abryksin@luxoft.com>
2020-10-11 14:26:44 +0300

    add comments model init

---
+++ internal/models/comment.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+// Comment model
+type Comment struct {
+	ID        uuid.UUID `json:"comment_id" db:"comment_id" validate:"omitempty,uuid"`
+	AuthorID  uuid.UUID `json:"author_id" db:"author_id" validate:"required"`
+	NewsID    uuid.UUID `json:"news_id" db:"news_id" validate:"required"`
+	Message   string    `json:"message" db:"message" validate:"required,gte=10"`
+	Likes     int64     `json:"likes" db:"likes" validate:"omitempty"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
