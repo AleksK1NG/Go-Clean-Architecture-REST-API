16bf9386f8837a2d6e07b1d4fb15bf4abe79c3a1
Alexander Bryksin <abryksin@luxoft.com>
2020-10-11 14:26:44 +0300

    add comments model init

--- migrations/01_create_initial_tables.up.sql
+++ migrations/01_create_initial_tables.up.sql
@@ -24,19 +24,34 @@ CREATE TABLE users
     gender       VARCHAR(20)                 NOT NULL DEFAULT 'male',
     postcode     INTEGER,
     birthday     DATE                                 DEFAULT NULL,
-    created_at   TIMESTAMP                   NOT NULL DEFAULT now(),
-    updated_at   TIMESTAMP                            DEFAULT current_timestamp,
-    login_date   TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT current_timestamp
+    created_at   TIMESTAMP WITH TIME ZONE    NOT NULL DEFAULT NOW(),
+    updated_at   TIMESTAMP WITH TIME ZONE             DEFAULT CURRENT_TIMESTAMP,
+    login_date   TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+
 
 CREATE TABLE news
 (
-    news_id    UUID PRIMARY KEY      DEFAULT uuid_generate_v4(),
-    author_id  UUID         NOT NULL REFERENCES users (user_id),
-    title      VARCHAR(250) NOT NULL check ( title <> '' ),
-    content    TEXT         NOT NULL check ( content <> '' ),
+    news_id    UUID PRIMARY KEY                  DEFAULT uuid_generate_v4(),
+    author_id  UUID                     NOT NULL REFERENCES users (user_id),
+    title      VARCHAR(250)             NOT NULL check ( title <> '' ),
+    content    TEXT                     NOT NULL check ( content <> '' ),
     image_url  VARCHAR(1024) check ( image_url <> '' ),
     category   VARCHAR(250),
-    created_at TIMESTAMP    NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP             DEFAULT current_timestamp
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE          DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE comments
+(
+    comment_id UUID PRIMARY KEY         DEFAULT uuid_generate_v4(),
+    author_id  UUID                                               NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
+    news_id    UUID                                               NOT NULL REFERENCES news (news_id) ON DELETE CASCADE,
+    message    VARCHAR(1024)                                      NOT NULL check ( message <> '' ),
+    likes      BIGINT                   DEFAULT 0,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX IF NOT EXISTS news_title_id_idx ON news (title);
