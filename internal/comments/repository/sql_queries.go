238e6cc4fd57c69439ff4d525a3d1eba7066695b
Alexander Bryksin <abryksin@luxoft.com>
2020-10-18 17:44:00 +0300

    add comments base

--- internal/comments/repository/sql_queries.go
+++ internal/comments/repository/sql_queries.go
@@ -7,11 +7,18 @@ const (
 
 	deleteComment = `DELETE FROM comments WHERE comment_id = $1`
 
-	getCommentByID = `SELECT comment_id, author_id, news_id, message, likes, updated_at 
-						FROM comments 
-						WHERE comment_id = $1`
+	getCommentByID = `SELECT concat(u.first_name, ' ', u.last_name) as author, u.avatar as avatar_url, c.message, c.likes, c.updated_at, c.author_id
+						FROM comments c
+        				LEFT JOIN users u on c.author_id = u.user_id
+						WHERE c.comment_id = $1`
 
 	getTotalCountByNewsId = `SELECT COUNT(comment_id) FROM comments WHERE news_id = $1`
 
-	getCommentsByNewsId = `SELECT * FROM comments WHERE news_id = $1 ORDER BY updated_at OFFSET $2 LIMIT $3`
+	// getCommentsByNewsId = `SELECT * FROM comments WHERE news_id = $1 ORDER BY updated_at OFFSET $2 LIMIT $3`
+
+	getCommentsByNewsId = `SELECT concat(u.first_name, ' ', u.last_name) as author, u.avatar as avatar_url, c.message, c.likes, c.updated_at, c.author_id
+							FROM comments c
+        					LEFT JOIN users u on c.author_id = u.user_id
+        					WHERE c.news_id = $1 
+							ORDER BY updated_at OFFSET $2 LIMIT $3`
 )
--- internal/models/comment.go
+++ internal/models/comment.go
@@ -16,12 +16,23 @@ type Comment struct {
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
 
+// Base Comment response DTO
+type CommentBase struct {
+	CommentID uuid.UUID `json:"comment_id" db:"comment_id" validate:"omitempty,uuid"`
+	AuthorID  uuid.UUID `json:"author_id" db:"author_id" validate:"required"`
+	Author    string    `json:"author" db:"author" validate:"required"`
+	AvatarURL *string   `json:"avatar_url" db:"avatar_url"`
+	Message   string    `json:"message" db:"message" validate:"required,gte=10"`
+	Likes     int64     `json:"likes" db:"likes" validate:"omitempty"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
 // All News response
 type CommentsList struct {
-	TotalCount int        `json:"total_count"`
-	TotalPages int        `json:"total_pages"`
-	Page       int        `json:"page"`
-	Size       int        `json:"size"`
-	HasMore    bool       `json:"has_more"`
-	Comments   []*Comment `json:"comments"`
+	TotalCount int            `json:"total_count"`
+	TotalPages int            `json:"total_pages"`
+	Page       int            `json:"page"`
+	Size       int            `json:"size"`
+	HasMore    bool           `json:"has_more"`
+	Comments   []*CommentBase `json:"comments"`
 }
