b6840c34d1d5a0e62e159cadc377653a830210c8
Alexander Bryksin <abryksin@luxoft.com>
2020-10-23 19:40:11 +0300

    add delete unused code

--- pkg/db/redis/conn.go
+++ pkg/db/redis/conn.go
@@ -8,6 +8,10 @@ import (
 	"github.com/gomodule/redigo/redis"
 	"golang.org/x/net/context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 // Redis db interface
@@ -43,8 +47,45 @@ func NewRedisClient(config *config.Config) *RedisClient {
 	}
 	pool := newPool(redisHost)
 
-	// cleanupHook()
+	cleanupHook(pool)
 	return &RedisClient{config: config, pool: pool}
+}
+
+func cleanupHook(pool *redis.Pool) {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	signal.Notify(c, syscall.SIGKILL)
+	go func() {
+		<-c
+		pool.Close()
+		os.Exit(0)
+	}()
+}
+
+func newPool(server string) *redis.Pool {
+
+	return &redis.Pool{
+
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		MaxActive:   12000,
+		//Wait:        true,
+
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
 }
 
 // Get pool
@@ -236,6 +277,7 @@ func (r *RedisClient) SetexJSONContext(ctx context.Context, key string, seconds 
 
 // Get JSON value
 func (r *RedisClient) GetJSONContext(ctx context.Context, key string, model interface{}) error {
+	ctx, _ = context.WithTimeout(context.Background(), time.Second*5)
 	conn, err := r.pool.GetContext(ctx)
 	if err != nil {
 		return err
