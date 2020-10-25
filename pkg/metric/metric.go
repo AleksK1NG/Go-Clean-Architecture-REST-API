8da66abfe14c541631c5f1186a15b26dcbbec346
Alexander Bryksin <abryksin@luxoft.com>
2020-10-25 19:23:12 +0300

    add docker compose local

--- pkg/metric/metric.go
+++ pkg/metric/metric.go
@@ -1,7 +1,7 @@
 package metric
 
 import (
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
--- pkg/utils/auth_utils.go
+++ pkg/utils/auth_utils.go
@@ -2,26 +2,9 @@ package utils
 
 import (
 	"context"
-	"github.com/AleksK1NG/api-mc/config"
 	"github.com/AleksK1NG/api-mc/pkg/httpErrors"
 	"github.com/AleksK1NG/api-mc/pkg/logger"
-	"github.com/labstack/echo"
-	"net/http"
-	"time"
 )
-
-// Set Auth Cookie with token
-func SetAuthCookieWithToken(c echo.Context, token string, config *config.Config) {
-	c.SetCookie(&http.Cookie{
-		Name:     config.Cookie.Name,
-		Value:    token,
-		Path:     "/",
-		Expires:  time.Now().Add(1 * 24 * time.Hour),
-		MaxAge:   config.Cookie.MaxAge,
-		Secure:   config.Cookie.Secure,
-		HttpOnly: config.Cookie.HttpOnly,
-	})
-}
 
 // Validate is user from owner of content
 func ValidateIsOwner(ctx context.Context, creatorId string) error {
