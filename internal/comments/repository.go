8a648f1b76a598ac2509423ef5c74bcdcbb6cc52
Alexander Bryksin <abryksin@luxoft.com>
2020-10-30 19:18:10 +0300

    add comments GetAllByNewsID refactor

--- internal/comments/repository.go
+++ internal/comments/repository.go
@@ -2,8 +2,8 @@ package comments
 
 import (
 	"context"
-	"github.com/AleksK1NG/api-mc/internal/dto"
 	"github.com/AleksK1NG/api-mc/internal/models"
+	"github.com/AleksK1NG/api-mc/pkg/utils"
 	"github.com/google/uuid"
 )
 
@@ -13,5 +13,5 @@ type Repository interface {
 	Update(ctx context.Context, comment *models.Comment) (*models.Comment, error)
 	Delete(ctx context.Context, commentID uuid.UUID) error
 	GetByID(ctx context.Context, commentID uuid.UUID) (*models.CommentBase, error)
-	GetAllByNewsID(ctx context.Context, query *dto.CommentsByNewsID) (*models.CommentsList, error)
+	GetAllByNewsID(ctx context.Context, newsID uuid.UUID, query *utils.PaginationQuery) (*models.CommentsList, error)
 }
