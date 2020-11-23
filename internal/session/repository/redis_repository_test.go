b820f07e8b7e93e6b4f69c4739eb05379a03aa68
Alexander Bryksin <abryksin@luxoft.com>
2020-11-23 16:24:49 +0300

    add session repo tests

---
+++ internal/session/repository/redis_repository_test.go
@@ -0,0 +1,70 @@
+package repository
+
+import (
+	"context"
+	"github.com/AleksK1NG/api-mc/internal/models"
+	"github.com/AleksK1NG/api-mc/internal/session"
+	"github.com/alicebob/miniredis"
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"log"
+	"testing"
+)
+
+func SetupRedis() session.SessRepository {
+	mr, err := miniredis.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+
+	sessRepository := NewSessionRepository(client, nil)
+	return sessRepository
+}
+
+func TestSessionRepo_CreateSession(t *testing.T) {
+	sessRepository := SetupRedis()
+
+	t.Run("CreateSession", func(t *testing.T) {
+		sessUUID := uuid.New()
+		sess := &models.Session{
+			SessionID: sessUUID.String(),
+			UserID:    sessUUID,
+		}
+		s, err := sessRepository.CreateSession(context.Background(), sess, 10)
+		require.NoError(t, err)
+		require.NotEqual(t, s, "")
+	})
+}
+
+func TestSessionRepo_GetSessionByID(t *testing.T) {
+	sessRepository := SetupRedis()
+
+	t.Run("GetSessionByID", func(t *testing.T) {
+		sessUUID := uuid.New()
+		sess := &models.Session{
+			SessionID: sessUUID.String(),
+			UserID:    sessUUID,
+		}
+		createdSess, err := sessRepository.CreateSession(context.Background(), sess, 10)
+		require.NoError(t, err)
+		require.NotEqual(t, createdSess, "")
+
+		s, err := sessRepository.GetSessionByID(context.Background(), createdSess)
+		require.NoError(t, err)
+		require.NotEqual(t, s, "")
+	})
+}
+
+func TestSessionRepo_DeleteByID(t *testing.T) {
+	sessRepository := SetupRedis()
+
+	t.Run("DeleteByID", func(t *testing.T) {
+		sessUUID := uuid.New()
+		err := sessRepository.DeleteByID(context.Background(), sessUUID.String())
+		require.NoError(t, err)
+	})
+}
